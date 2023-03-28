f62073216a0d46b9cdbe09a8abbb9177cbeb3190
Nwachukwu-Uzor <uzor1997@gmail.com>
2023-03-28 11:12:04 +0100

    setups up plugins and dependencies for testing

---
+++ src/components/Hello.spec.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+
+import { Hello } from "./Hello";
+
+it('Should render "Hello World"', () => {
+  render(<Hello />);
+  const component = screen.getByText(/Hello World/);
+  screen.debug();
+  expect(component).toBeInTheDocument();
+});
---
+++ src/components/sum.spec.ts
@@ -0,0 +1,5 @@
+import { sum } from "./sum";
+
+it("Should return the sum of 2 and 5 to be 7", () => {
+  expect(sum(2, 5)).toBe(7);
+});
