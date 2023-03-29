0e00d6e37db25da6c7ef8e3a1fdc56955f7d4318
Nwachukwu-Uzor <uzor1997@gmail.com>
2023-03-29 13:12:02 +0100

    replaces call on fireevent with userevent where possible

--- src/components/Counter.spec.tsx
+++ src/components/Counter.spec.tsx
@@ -1,5 +1,6 @@
 import { screen, render, fireEvent } from "@testing-library/react";
 import { Counter } from "./Counter";
+import user from "@testing-library/user-event";
 
 describe("Counter", () => {
   const setup = () =>
@@ -19,62 +20,64 @@ describe("Counter", () => {
     expect(screen.getByText(/Current Count: 0/)).toBeInTheDocument();
   });
 
-  it("Increment Counter to 2 when increment button is clicked twice", () => {
+  it("Increment Counter to 2 when increment button is clicked twice", async () => {
     setup();
     const incrementButton = screen.getByRole("button", {
       name: "increment-counter",
     });
-    fireEvent.click(incrementButton);
-    fireEvent.click(incrementButton);
+    await user.click(incrementButton);
+    await user.click(incrementButton);
     expect(screen.getByText(/Current Count: 2/)).toBeInTheDocument();
   });
 
-  it("Reduce Counter to -2 when increment button is clicked twice", () => {
+  it("Reduce Counter to -2 when increment button is clicked twice", async () => {
     setup();
 
     const decrementButton = screen.getByRole("button", {
       name: "decrease-counter",
     });
-    fireEvent.click(decrementButton);
-    fireEvent.click(decrementButton);
+    await user.click(decrementButton);
+    await user.click(decrementButton);
     expect(screen.getByText(/Current Count: -2/)).toBeInTheDocument();
   });
 
-  it("increment value input should revert to 1 when blurred if the value entered is less than 1", () => {
+  it("increment value input should revert to 1 when blurred if the value entered is less than 1", async () => {
     setup();
 
     const incrementBox = screen.getByLabelText("incrementor-value");
-    fireEvent.change(incrementBox, { target: { value: "0" } });
+    await user.type(incrementBox, "{backspace}0");
+
     fireEvent.blur(incrementBox);
     expect(screen.getByLabelText("incrementor-value")).toHaveValue("1");
   });
 
-  it("incrementor value should be 12 when a user types '1a2'", () => {
+  it("incrementor value should be 12 when a user types '1a2'", async () => {
     setup();
     const incrementBox = screen.getByLabelText("incrementor-value");
-    fireEvent.change(incrementBox, { target: { value: "1a2" } });
+    await user.type(incrementBox, "{backspace}1a2");
     expect(screen.getByLabelText("incrementor-value")).toHaveValue("12");
   });
 
-  it("the count should increase by the incrementor value, count should be 5 when the incrementor value is 5 and the increment button is clicked", () => {
+  it("the count should increase by the incrementor value, count should be 5 when the incrementor value is 5 and the increment button is clicked", async () => {
     setup();
     const incrementBox = screen.getByLabelText("incrementor-value");
     const incrementButton = screen.getByRole("button", {
       name: "increment-counter",
     });
-    fireEvent.change(incrementBox, { target: { value: "5" } });
-    fireEvent.click(incrementButton);
+    await user.type(incrementBox, "{backspace}5");
+
+    await user.click(incrementButton);
     expect(screen.getByText(/Current Count: 5/)).toBeInTheDocument();
   });
 
-  it("the count should increase by the incrementor value, count should be -5 when the incrementor value is 5 and the decrement button is clicked", () => {
+  it("the count should increase by the incrementor value, count should be -5 when the incrementor value is 5 and the decrement button is clicked", async () => {
     setup();
     const incrementBox = screen.getByLabelText("incrementor-value");
     const decrementButton = screen.getByRole("button", {
       name: "decrease-counter",
     });
-    fireEvent.change(incrementBox, { target: { value: "5" } });
-    fireEvent.click(decrementButton);
+    await user.type(incrementBox, "{backspace}5");
+    await user.click(decrementButton);
     expect(screen.getByText(/Current Count: -5/)).toBeInTheDocument();
   });
 });
